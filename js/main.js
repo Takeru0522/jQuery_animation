e4e041d1eb76ff580bc8bb9a2155249adc0d56b3
Takeru Kayamori <takeru.kayamori@gmail.com>
2020-07-12 19:27:06 +0800

    active slider successfully

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 console.log('Hello');
 
+
+
+// $(document).ready(function(){
+//     $('.single-item').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         adaptiveHeight: true
+//     });
+// });
+$('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+});
+
+$('.single-item').slick();
+
+$('.multiple-items').slick({
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+    infinite: true,
+    slidesToShow: 1,
+    // slidesToScroll: 1
+});
+
+
+// aaaaaaaaaaaa
 const item = $('.item');
 
 item.on('click', function() {
